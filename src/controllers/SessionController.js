e70955fbae3e895eef4bfe5f8372bdfa98c9b299
ISABEL <cg3026337@labA204m07.IFSP>
2024-05-13 08:51:51 -0300

    Criação dos Models

---
+++ src/controllers/SessionController.js
@@ -0,0 +1,16 @@
+/*
+    index (listAll): listagem de sessoes
+    store(add): criar uma sessao
+    show (list): quando queremos listar uma unica sessao
+    upda: quando queremos alterar alguma sessao
+    destroy (delete): quando queremos ddeletar uma sessao
+*/
+
+import User from '../models/User'
+import * as Yup from  'yup'
+
+class SessionController {
+    async store(req, res){
+        
+    }
+}
